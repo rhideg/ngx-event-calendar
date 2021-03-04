947263fbc2f20129eab842942883d2f1a5019cc8
Albertino Júnior <jr.albertino@outlook.com>
2021-03-03 21:01:21 -0300

    Create monthsPT-BR.ts

---
+++ projects/ngx-event-calendar/src/lib/locale/monthsPT-BR.ts
@@ -0,0 +1,14 @@
+export const mounthsPT-BR = [
+  { id: 0, name: 'Janeiro' },
+  { id: 1, name: 'Fevereiro' },
+  { id: 2, name: 'Março' },
+  { id: 3, name: 'Abril' },
+  { id: 4, name: 'Maio' },
+  { id: 5, name: 'Junho' },
+  { id: 6, name: 'Julho' },
+  { id: 7, name: 'Agosto' },
+  { id: 8, name: 'Setembro' },
+  { id: 9, name: 'Outubro' },
+  { id: 10, name: 'Novembro' },
+  { id: 11, name: 'Dezembro' }
+];
